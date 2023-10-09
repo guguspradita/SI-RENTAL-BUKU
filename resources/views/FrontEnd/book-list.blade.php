1313921db67fb80bfaad02896a458088aad7ea97
guguspradita <guguspradita19@gmail.com>
2023-10-09 21:06:06 +0700

    Show books in database

--- resources/views/FrontEnd/book-list.blade.php
+++ resources/views/FrontEnd/book-list.blade.php
@@ -7,11 +7,15 @@
         @foreach ($books as $item)
             <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
                 <div class="card">
-                    <img src="{{ asset('images/no_cover.jpg') }}" class="card-img-top" alt="..." height="250px">
+                    <img src="{{ $item->cover != '' ? asset('storage/cover/' . $item->cover) : asset('images/no_cover.jpg') }}"
+                        class="card-img-top" draggable="false" height="250px">
                     <div class="card-body">
                         <h5 class="card-title">{{ $item->book_code }}</h5>
                         <p class="card-text">{{ $item->title }}</p>
-                        <p class="card-text text-end">{{ $item->status }}</p>
+                        <p
+                            class="card-text text-end fw-bold {{ $item->status == 'in stock' ? 'text-success' : 'text-danger' }}">
+                            {{ $item->status }}
+                        </p>
                     </div>
                 </div>
             </div>
