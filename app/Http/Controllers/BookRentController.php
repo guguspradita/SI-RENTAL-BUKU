c222e7c050e8c903546b69cc736d81951f929b0c
guguspradita <guguspradita19@gmail.com>
2023-10-11 11:29:39 +0700

    desaign book rent form and fetch database

--- app/Http/Controllers/BookRentController.php
+++ app/Http/Controllers/BookRentController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class BookRentController extends Controller
 {
     public function index()
     {
-        return view('BackEnd.book-rent');
+        $users = User::where('id', '!=', 1)->get(); // get() -> lebih dari satu data
+        $books = Book::all();
+        return view('BackEnd.book-rent', ['users' => $users, 'books' => $books]);
     }
 }
--- resources/views/BackEnd/book-rent.blade.php
+++ resources/views/BackEnd/book-rent.blade.php
@@ -2,5 +2,39 @@
 @section('title', 'Book Rent')
 
 @section('content')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
+        <h1 class="mb-5">Book Rent Form</h1>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="user" class="form-label">User</label>
+                <select name="user" id="user" class="form-control inputbox">
+                    <option value="">Select User</option>
+                    @foreach ($users as $item)
+                        <option value="{{ $item->id }}">{{ $item->username }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="book" class="form-label">Book</label>
+                <select name="book" id="book" class="form-control inputbox">
+                    <option value="">Select Book</option>
+                    @foreach ($books as $item)
+                        <option value="{{ $item->id }}">{{ $item->title }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary w-100">Submit</button>
+            </div>
+        </form>
+    </div>
 
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.inputbox').select2();
+        });
+    </script>
 @endsection
