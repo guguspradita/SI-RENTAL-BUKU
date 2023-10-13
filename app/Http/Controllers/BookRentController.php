ce9d67454428cbe46f0700e95ec0c4dad30bd28c
guguspradita <guguspradita19@gmail.com>
2023-10-13 14:29:48 +0700

    save data rent logs - with db:transactions

--- app/Http/Controllers/BookRentController.php
+++ app/Http/Controllers/BookRentController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\Book;
+use App\Models\RentLogs;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class BookRentController extends Controller
@@ -19,8 +21,8 @@ class BookRentController extends Controller
 
     public function store(Request $request)
     {
-        $request['rent_date'] = Carbon::now()->isoFormat('dddd, D MMMM Y');
-        $request['return_date'] = Carbon::now()->addDay(3)->isoFormat('dddd, D MMMM Y');
+        $request['rent_date'] = Carbon::now()->toDateTimeString();
+        $request['return_date'] = Carbon::now()->addDay(3)->toDateTimeString();
 
         $book = Book::findOrFail($request->book_id)->only('status'); // hanya mengambil status dari book
         // dd($book);
@@ -28,7 +30,23 @@ class BookRentController extends Controller
             Session::flash('message', 'Cannot Rent, the book is not available!');
             Session::flash('alert-class', 'alert-danger');
             return redirect('book-rent');
+        } else {
+            try {
+                DB::beginTransaction();
+                // process insert to rent_logs table
+                RentLogs::create($request->all());
+                // process update book table
+                $book = Book::findOrFail($request->book_id);
+                $book->status = 'not available';
+                $book->save();
+                DB::commit();
+
+                Session::flash('message', 'Rent book success!!!');
+                Session::flash('alert-class', 'alert-success');
+                return redirect('book-rent');
+            } catch (\Throwable $th) {
+                DB::rollBack();
+            }
         }
-        dd('book boleh dipinjam!');
     }
 }
